ef1b570ff956658bb28bb9973d4dbf5d6d5304c9
dii93 <di_1993@hotmail.com>
2023-10-18 19:35:48 -0300

    configuracoes de deploy

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,7 +8,7 @@ import Private from "./Private";
 function RoutesApp() {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/listaDeTarefas" element={<Home />} />
             <Route path="/register" element={<Register />} />
             <Route path="/admin" element={<Private> <Admin /> </Private>} />
         </Routes>
